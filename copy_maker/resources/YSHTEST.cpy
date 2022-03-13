e5f974958ddf93324307146ebc45184bfd9a42fc
Seb_HERT <55281709+Sebibebi67@users.noreply.github.com>
2022-03-13 14:55:11 +0100

    sync git and workspace

---
+++ copy_maker/resources/YSHTEST.cpy
@@ -0,0 +1,32 @@
+      * ============================================================== *
+      *Nom de Copy : YBCARCA
+      *Version     : V(1.00)
+      *Description : 
+      *Date        : 12/03/2022
+      *Auteur      : 
+      *Longueur    : 100
+      * ============================================================== *
+
+      *01 :BCARCA:
+
+      *    TEST1
+           05 :BCARCA:-DATA1                            PIC X(10).
+
+      *    TEST1
+           05 :BCARCA:-DATA2                            PIC 9(09).
+
+      *        TEST1
+               10 :BCARCA:-DATA2                        PIC 9(09).
+
+      *            TEST1
+                   15 :BCARCA:-DATA2                    PIC 9(09).
+
+      *    OUI
+           88 :BCARCA:-DATA1-OUI                        VALUE 'OUI'.
+
+      *    NON
+           88 :BCARCA:-DATA1-NON                        VALUE 'NON'.
+
+      *    TEST2
+           05 :BCARCA:-DATA3                            PIC X(15).
+
---
+++ copy_maker/src/main.js
@@ -0,0 +1,538 @@
+//==================================== main.js ===================================//
+
+
+//--------------------------------- Description ----------------------------------//
+//
+// This file contains all methods to translate a CSV file into a COBOL Copy
+//
+//--------------------------------------------------------------------------------//
+
+
+//----------------------------------- Authors ------------------------------------//
+//
+// Sébastien HERT
+//
+//--------------------------------------------------------------------------------//
+
+
+//------------------------------------ Errors ------------------------------------//
+//
+const errorCSV = 'Error : the given file is not a CSV file.';
+const errorBrowser = 'Error : the browser does not support "FileReader".\nPlease use another one.';
+// 
+//--------------------------------------------------------------------------------//
+
+
+//------------------------------- Global Variables -------------------------------//
+//
+var csvFile;
+var processOk = true;
+var outputDivision;
+var copy;
+var copyName;
+// 
+//--------------------------------------------------------------------------------//
+
+
+//------------------------------- Global Constants -------------------------------//
+//
+const regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv)$/;
+const parser = ';';
+
+const border = ' ============================================================== *';
+const cName    		= 'Nom de Copy : ';
+const cVersion 		= 'Version     : ';
+const cDescription 	= 'Description : ';
+const cDate			= 'Date        : ';
+const cAuthor		= 'Auteur      : ';
+const cLength		= 'Longueur    : ';
+
+const margin = '       ';
+const commentedMargin = '      *';
+const tab = '    ';
+const space = ' '; 
+const point = '.';
+
+// 
+//--------------------------------------------------------------------------------//
+
+
+//------------------------------------- Main -------------------------------------//
+//
+
+// this part contains the event Listener used to display the choosen file's name
+const actualBtn = document.getElementById('CopyCSV');
+const fileChosen = document.getElementById('chosenFile');
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
+
+/**
+ * Description : Generates the COBOL Copy with the given file
+ *
+ * Input :
+ * - None
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function generateCopy() {
+
+	// Getting the output division and clearing it
+	outputDivision = document.getElementById('output');
+	while (outputDivision.firstChild) {
+		outputDivision.firstChild.remove();
+	}
+
+	// Getting the File
+	csvFile = document.getElementById("CopyCSV");
+
+	// Getting the copy Name
+	copyName = document.getElementById("CopyName").value;
+
+	copy = new FileCopy(copyName);
+
+	// Checking fileName
+	checkFile();
+
+	// Parsing the file
+	if (processOk){ parseFile(); }
+}
+
+/**
+ * Description : Allows the user to copy to keyboard the generated Copy.
+ * It uses execCommand, which is deprecated, but any equivalent existed,
+ * so it has been decided to leave it in.
+ *
+ * Input :
+ * - None
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function copyToClipboard(){
+	var range = document.createRange();
+	range.selectNode(document.getElementById("output"));
+	window.getSelection().removeAllRanges(); // clear current selection
+	window.getSelection().addRange(range); // to select text
+	document.execCommand("copy");
+	window.getSelection().removeAllRanges();
+}
+
+
+
+// 
+//--------------------------------------------------------------------------------//
+
+//---------------------------------- Functions -----------------------------------//
+//
+/**
+ * Description : Verifies if the file is a CSV
+ *
+ * Input :
+ * - None
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function checkFile() {
+	if (!regex.test(csvFile.value.toLowerCase())){
+		displayError(errorCSV);
+	}
+
+	if (typeof (FileReader) == "undefined") {
+		displayError(errorBrowser);
+	}
+}
+
+/**
+ * Description : Parses the file into lines
+ *
+ * Input :
+ * - None
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function parseFile(){
+	var reader = new FileReader();
+
+	reader.onload = function(e){
+		var content = reader.result;
+		var lines = content.split('\n');
+
+		// For each line
+		for (var count = 0; count < lines.length; count++) {
+			copy.addLine(lines[count]);
+		}
+		displayOutput();	
+	}
+	reader.readAsText(csvFile.files[0]);
+}
+
+/**
+ * Description : Fills the output Division
+ *
+ * Input :
+ * - None
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function displayOutput(){
+	outputDivision.className = 'output-visible';
+	outputDivision.appendChild(copy.generateHeader());
+	outputDivision.appendChild(document.createElement("br"));
+	copy.lines.forEach(line => {
+		outputDivision.appendChild(line.generateLine());
+		outputDivision.appendChild(document.createElement("br"));
+	});
+}
+
+/**
+ * Description : Displays an error on the console and on the output
+ *
+ * Input :
+ * - error : the error as a String
+ *
+ * Output :
+ * - None
+ *
+ * Authors :
+ * - Sébastien HERT
+ */
+function displayError(error){
+	console.error(error);
+	processOk = false;
+}
+// 
+//--------------------------------------------------------------------------------//
+
+
+//================================ Class FileCopy ================================//
+
+//--------------------------------- Description ----------------------------------//
+//
+// Contains the structure of the COBOL Copy File
+//
+//--------------------------------------------------------------------------------//
+
+class FileCopy {
+
+	copyName = 'Yxxxxxx';
+	copyNameAsParameter;
+	version = 'V(1.00)';
+	description = '';
+	length = '100';
+	author = '';
+	date = 'JJ/MM/AAAA';
+	previousIndent = '';
+
+	lines = [];
+
+
+//--------------------------------- Constructor ----------------------------------//
+
+	constructor(copyName){
+		if (copyName.length == 7){
+			this.copyName = copyName.toUpperCase();
+		}
+		this.copyNameAsParameter = ':' + this.copyName.substring(1,7) + ':';
+
+		this.date = this.getDate()
+	}
+
+//--------------------------------------------------------------------------------//
+
+	/**
+	 * Description : Adds a line to the Copy
+	 *
+	 * Input :
+	 * - the line as a String
+	 *
+	 * Output :
+	 * - None
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	addLine (lineAsString) {
+		// Parsing the line
+		var parameters = lineAsString.split(parser);
+		console.log(parameters);
+		if (parameters.length > 1){
+			// Creating a new Line
+			var line = new Line(
+				parameters[0]?.trim(),
+				this.copyNameAsParameter + '-' + parameters[1]?.trim(),
+				parameters[2]?.trim(),
+				parameters[3]?.trim(),
+				this.previousIndent);
+			
+			// Preventing indentation after reading a level 88 or 77
+			if (line.level != '88' && line.level != '77'){
+				this.previousIndent = line.indent;
+			}
+	
+			// Adding the line to the list
+			this.lines.push(line);
+		}
+
+
+	}
+
+	/**
+	 * Description : Generates the copy header
+	 *
+	 * Input :
+	 * - None
+	 *
+	 * Output :
+	 * - The HTML element which contains the header
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	generateHeader(){
+		var header = document.createElement("div")
+
+		var commentedLine1 = document.createElement("span");
+		commentedLine1.className = 'comment';
+		commentedLine1.textContent = commentedMargin
+									+ border;
+
+		var copyNameLine = document.createElement("span");
+		copyNameLine.className = 'comment';
+		copyNameLine.textContent = commentedMargin
+								 + cName
+								 + this.copyName;
+
+		var versionLine = document.createElement("span");
+		versionLine.className = 'comment';
+		versionLine.textContent = commentedMargin
+								+ cVersion
+								+ this.version;	
+
+		var descriptionLine = document.createElement("span");
+		descriptionLine.className = 'comment';
+		descriptionLine.textContent = commentedMargin
+									+ cDescription
+									+ this.description;
+
+		var dateLine = document.createElement("span");
+		dateLine.className = 'comment';
+		dateLine.textContent = commentedMargin
+							 + cDate
+							 + this.date;
+
+		var authorLine = document.createElement("span");
+		authorLine.className = 'comment';
+		authorLine.textContent = commentedMargin
+							   + cAuthor
+							   + this.author;
+
+		var lengthLine = document.createElement("span");
+		lengthLine.className = 'comment';
+		lengthLine.textContent = commentedMargin
+							   + cLength
+							   + this.length;
+
+		var firstLine = document.createElement("span");
+		firstLine.className = 'comment';
+		firstLine.textContent = commentedMargin
+							  + '01'
+							  + space
+							  + this.copyNameAsParameter;
+
+		var commentedLine2 = document.createElement("span");
+		commentedLine2.className = 'comment';
+		commentedLine2.textContent = commentedMargin
+									+ border;
+
+		header.appendChild(commentedLine1);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(copyNameLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(versionLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(descriptionLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(dateLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(authorLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(lengthLine);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(commentedLine2);
+		header.appendChild(document.createElement("br"));
+		header.appendChild(document.createElement("br"));
+		header.appendChild(firstLine);
+
+		return header;
+	}
+
+	/**
+	 * Description : Calculates the current date with the DD/MM/YYYY format
+	 *
+	 * Input :
+	 * - None
+	 *
+	 * Output :
+	 * - the current date as a string with the DD/MM/YYYY foramat
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	getDate(){
+		var today = new Date();
+		var dd = String(today.getDate()).padStart(2, '0');
+		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+		var yyyy = today.getFullYear();
+		
+		today = dd + '/' + mm + '/' + yyyy;
+		return today;
+	}
+}
+
+//================================== Class Line ==================================//
+
+
+//--------------------------------- Description ----------------------------------//
+//
+// Contains the description of a line
+//
+//--------------------------------------------------------------------------------//
+
+class Line {
+	level;
+	name;
+	type;
+	description;
+	indent = '';
+	previousIndent= '';
+
+//--------------------------------- Constructor ----------------------------------//
+
+	constructor(level, name, type, description, previousIndent = ''){
+		this.level = level;
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.previousIndent = previousIndent;
+		this.indent += this.getIndent();
+	}
+
+//--------------------------------------------------------------------------------//
+
+	/**
+	 * Description : Generates both description and data lines
+	 *
+	 * Input :
+	 * - None
+	 *
+	 * Output :
+	 * - the HTML division which contains both lines
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	generateLine(){
+		var smallDiv = document.createElement("div")
+		var lineDescription = document.createElement("span");
+		lineDescription.className = 'comment';
+		lineDescription.textContent = commentedMargin
+									+ this.indent
+									+ this.description;
+
+
+		var lineContent = document.createElement("span");
+		lineContent.className = 'copyLine';
+		lineContent.textContent = margin
+								+ this.indent
+								+ this.level
+								+ space
+								+ this.name
+								+ this.getSpaces()
+								+ this.type
+								+ point;
+
+		smallDiv.appendChild(lineDescription);
+		smallDiv.appendChild(document.createElement("br"));
+		smallDiv.appendChild(lineContent);
+
+		return smallDiv;
+	}
+
+	/**
+	 * Description : Calculates the current indent
+	 *
+	 * Input :
+	 * - None
+	 *
+	 * Output :
+	 * - the indent as a String of spaces
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	getIndent(){
+		switch (this.level) {
+			case '05':
+				return tab
+			case '10':
+				return tab + tab 
+			case '15':
+				return tab + tab + tab
+			case '20':
+				return tab + tab + tab + tab
+			case '77':
+			case '88':
+				return this.previousIndent + tab;
+			default:
+				return '';
+		}
+	}
+
+	/**
+	 * Description : generates spaces between the parameter name and it's type/value
+	 *
+	 * Input :
+	 * - None
+	 *
+	 * Output :
+	 * - multiple spaces
+	 *
+	 * Authors :
+	 * - Sébastien HERT
+	 */
+	getSpaces(){
+		var spaces = ''
+		var length = 56
+				   - margin.length
+				   - this.indent.length
+				   - this.level.length
+				   - 1
+				   - this.name.length;
+		for (let index = 0; index < length; index++) {
+			spaces = spaces + space;			
+		}
+		return spaces;
+	}
+}
+
+//================================================================================//
